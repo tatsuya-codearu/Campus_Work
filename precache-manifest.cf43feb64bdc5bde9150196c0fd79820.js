cb8c46cd21fc83bd78718060a42a88cc693a2af6
tatsuya-codearu <85080895+tatsuya-codearu@users.noreply.github.com>
2021-11-17 16:31:07 +0200

    Add files via upload

---
+++ precache-manifest.cf43feb64bdc5bde9150196c0fd79820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a4cc5cbb04526d30017dc292d054a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95f7f0b62bc83491f619",
+    "url": "/static/js/2.53aef304.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.53aef304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd675de175a380296425",
+    "url": "/static/js/main.2e8d2a41.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "4d77cd77c5c0ff01523d5a214cc48f37",
+    "url": "/static/media/commerce.4d77cd77.png"
+  }
+]);
